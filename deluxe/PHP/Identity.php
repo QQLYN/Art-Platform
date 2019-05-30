3d95c82ed9ac825ecca131ef55150d29eb65f233
tutu0511 <amy0511tw@gmail.com>
2019-05-30 19:36:58 +0800

    Update Identity.php

--- deluxe/PHP/Identity.php
+++ deluxe/PHP/Identity.php
@@ -1,6 +1,19 @@
 <?php
-	$identity = $_POST['identity'];
-	$json = array('msg' => $identity);
-	echo $identity;
-	echo json_encode($json);
+	$Identity = json_encode($_POST['Value']);
+	//echo $Identity;
+	$Link = mysqli_connect('localhost', 'ARTSA', 'artsa108');
+	mysqli_select_db($Link,'artsa')or die($connect_error);
+	$data = mysqli_query($Link,"UPDATE `test` SET `Identity`='".$Identity."'");
+	$result = mysqli_query($Link,"SELECT `Identity` FROM `test`");
+	if(!$result)
+	{
+		echo ("Error: ".mysqli_error($Link));
+		exit();
+	}
+	while ($row = mysqli_fetch_array($result)) {
+		//echo 'Identity = ';
+		echo json_encode(array('Identity' => $row['Identity']));
+	}
+	
+	mysqli_close($Link);
 ?>
