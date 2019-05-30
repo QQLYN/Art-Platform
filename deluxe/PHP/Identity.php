6f1cd84550c7b4ee9ba9ee4eedfddbe2a9ccf750
tutu0511 <amy0511tw@gmail.com>
2019-05-30 20:22:28 +0800

    Update Identity.php

--- deluxe/PHP/Identity.php
+++ deluxe/PHP/Identity.php
@@ -1,10 +1,10 @@
 <?php
-	$Identity = json_encode($_POST['Value']);
+	$Identity = json_encode($_POST['role']);
 	//echo $Identity;
 	$Link = mysqli_connect('localhost', 'ARTSA', 'artsa108');
 	mysqli_select_db($Link,'artsa')or die($connect_error);
-	$data = mysqli_query($Link,"UPDATE `test` SET `Identity`='".$Identity."'");
-	$result = mysqli_query($Link,"SELECT `Identity` FROM `test`");
+	$data = mysqli_query($Link,"UPDATE `test` SET `role`=".$Identity);
+	$result = mysqli_query($Link,"SELECT `role` FROM `test`");
 	if(!$result)
 	{
 		echo ("Error: ".mysqli_error($Link));
@@ -12,7 +12,15 @@
 	}
 	while ($row = mysqli_fetch_array($result)) {
 		//echo 'Identity = ';
-		echo json_encode(array('Identity' => $row['Identity']));
+		if($row['role'] == 1)
+		{
+			echo "COUMSER";
+		}
+		if($row['role'] == 2)
+		{
+			echo "PHOTOGRAPHER";
+		}
+		//echo json_encode(array('Identity' => $row['Identity']));
 	}
 	
 	mysqli_close($Link);
