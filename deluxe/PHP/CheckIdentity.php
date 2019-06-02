80b5d218e72874313aeaf1b1d138b551334de9cf
tutu0511 <amy0511tw@gmail.com>
2019-06-02 22:02:30 +0800

    Update CheckIdentity.php

--- deluxe/PHP/CheckIdentity.php
+++ deluxe/PHP/CheckIdentity.php
@@ -7,7 +7,7 @@
 	echo $role;
 	$Link = mysqli_connect('localhost', 'root', '1234');
 	mysqli_select_db($Link,'artsa')or die($connect_error);
-	$result = mysqli_query($Link,"SELECT `role` FROM `account`where `Mail` = '".$Mail."'");
+	$result = mysqli_query($Link,"SELECT `role` FROM `account`where `Mail` = ".$Mail);
 	if(!$result)
 	{
 		echo ("Error: ".mysqli_error($Link));
