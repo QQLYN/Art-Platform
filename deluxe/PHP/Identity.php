6b1500dddc57c1914343cff546a2e1f0ce9fa4d2
tutu0511 <amy0511tw@gmail.com>
2019-06-02 21:08:17 +0800

    Update Identity.php

--- deluxe/PHP/Identity.php
+++ deluxe/PHP/Identity.php
@@ -4,12 +4,12 @@
 	$_SESSION[role] = $Identity;
 	$Mail = json_encode(@$_POST["Mail"]);
 	//echo $_SESSION[role];
-	$Link = mysqli_connect('localhost', 'ARTSA', 'artsa108');
+	$Link = mysqli_connect('localhost', 'root', '1234');
 	mysqli_select_db($Link,'artsa')or die($connect_error);
 	//$data = mysqli_query($Link,"UPDATE `account` SET `role`=".$Identity."where `Mail` = '".$Mail."'" );
 	//$result = mysqli_query($Link,"SELECT `role` FROM `account`where `Mail` = '".$Mail."'");
-	$data = mysqli_query($Link,"UPDATE `account` SET `role`=".$Identity);
-	$result = mysqli_query($Link,"SELECT `role` FROM `account");
+	$data = mysqli_query($Link,"UPDATE `account` SET `role`=".$Identity."where `Mail` = '".$Mail."'");
+	$result = mysqli_query($Link,"SELECT `role` FROM `account` where `Mail` = '".$Mail."'");
 	if(!$result)
 	{
 		echo ("Error: ".mysqli_error($Link));
